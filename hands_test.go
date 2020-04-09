cef8aff6fea55f598ce428edab75321394e33fb3
Duanckham <duanckham@gmail.com>
2020-04-09 14:12:10 +0800

    MISC

--- hands_test.go
+++ hands_test.go
@@ -2,6 +2,7 @@ package hands
 
 import (
 	"context"
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -23,264 +24,265 @@ func TestSimpleCase(t *testing.T) {
 }
 
 func TestFastestOnly(t *testing.T) {
-	n := 0
+	var n int32
 	controller := New()
 
 	controller.Do(func(ctx context.Context) error {
 		time.Sleep(time.Duration(10) * time.Millisecond)
-		n++
+		atomic.AddInt32(&n, 1)
 		return nil
 	})
 
 	controller.Do(func(ctx context.Context) error {
 		time.Sleep(time.Duration(10) * time.Millisecond)
-		n++
+		atomic.AddInt32(&n, 1)
 		return nil
 	})
 
 	controller.Do(func(ctx context.Context) error {
-		n += 5
+		atomic.AddInt32(&n, 5)
 		return nil
 	})
 
 	controller.Run(Fastest())
 
-	assert.Equal(t, n, 5)
+	assert.Equal(t, atomic.LoadInt32(&n), int32(5))
 }
 
 func TestFastestAndRunAll(t *testing.T) {
-	n := 0
+	var n int32
 	controller := New()
 
 	controller.Do(func(ctx context.Context) error {
 		time.Sleep(time.Duration(10) * time.Millisecond)
-		n++
+		atomic.AddInt32(&n, 1)
 		return nil
 	})
 
 	controller.Do(func(ctx context.Context) error {
 		time.Sleep(time.Duration(10) * time.Millisecond)
-		n++
+		atomic.AddInt32(&n, 1)
 		return nil
 	})
 
 	controller.Do(func(ctx context.Context) error {
-		n += 5
+		atomic.AddInt32(&n, 5)
 		return nil
 	})
 
 	controller.RunAll(Fastest())
 
-	assert.Equal(t, n, 5)
+	assert.Equal(t, atomic.LoadInt32(&n), int32(5))
 	time.Sleep(time.Duration(100) * time.Millisecond)
-	assert.Equal(t, n, 7)
+	assert.Equal(t, atomic.LoadInt32(&n), int32(7))
 }
 
 func TestPercentageOnly(t *testing.T) {
-	n := 0
+	var n int32
 	controller := New()
 
 	controller.Do(func(ctx context.Context) error {
 		time.Sleep(time.Duration(10) * time.Millisecond)
-		n++
+		atomic.AddInt32(&n, 1)
 		return nil
 	})
 
 	controller.Do(func(ctx context.Context) error {
 		time.Sleep(time.Duration(20) * time.Millisecond)
-		n++
+		atomic.AddInt32(&n, 1)
 		return nil
 	})
 
 	controller.Do(func(ctx context.Context) error {
 		time.Sleep(time.Duration(30) * time.Millisecond)
-		n++
+		atomic.AddInt32(&n, 1)
 		return nil
 	})
 
 	controller.Do(func(ctx context.Context) error {
 		time.Sleep(time.Duration(40) * time.Millisecond)
-		n++
+		atomic.AddInt32(&n, 1)
 		return nil
 	})
 
 	controller.Run(Percentage(0.5))
 
-	assert.Equal(t, n, 2)
+	assert.Equal(t, atomic.LoadInt32(&n), int32(2))
 }
 
 func TestPercentageAndRunAll(t *testing.T) {
-	n := 0
+	var n int32
 	controller := New()
 
 	controller.Do(func(ctx context.Context) error {
 		time.Sleep(time.Duration(10) * time.Millisecond)
-		n++
+		atomic.AddInt32(&n, 1)
 		return nil
 	})
 
 	controller.Do(func(ctx context.Context) error {
 		time.Sleep(time.Duration(20) * time.Millisecond)
-		n++
+		atomic.AddInt32(&n, 1)
 		return nil
 	})
 
 	controller.Do(func(ctx context.Context) error {
 		time.Sleep(time.Duration(30) * time.Millisecond)
-		n++
+		atomic.AddInt32(&n, 1)
 		return nil
 	})
 
 	controller.Do(func(ctx context.Context) error {
 		time.Sleep(time.Duration(40) * time.Millisecond)
-		n++
+		atomic.AddInt32(&n, 1)
 		return nil
 	})
 
 	controller.Run(Percentage(0.5))
 
-	assert.Equal(t, n, 2)
+	assert.Equal(t, atomic.LoadInt32(&n), int32(2))
 	time.Sleep(time.Duration(100) * time.Millisecond)
-	assert.Equal(t, n, 4)
+	assert.Equal(t, atomic.LoadInt32(&n), int32(4))
 }
 
 func TestBetweenOnly(t *testing.T) {
-	n := 0
+	var n int32
 	controller := New()
 
 	for i := 0; i < 5; i++ {
 		controller.Do(func(ctx context.Context) error {
-			n++
+			atomic.AddInt32(&n, 1)
 			return nil
 		}, P(int32(i)))
 	}
 
 	controller.Run(Between(2, 3))
 
-	assert.Equal(t, n, 2)
+	assert.Equal(t, atomic.LoadInt32(&n), int32(2))
 }
 
 func TestBetweenAndRunAll(t *testing.T) {
-	n := 0
+	var n int32
 	controller := New()
 
 	for i := 0; i < 5; i++ {
 		controller.Do(func(ctx context.Context) error {
-			n++
+			atomic.AddInt32(&n, 1)
 			return nil
 		}, P(int32(i)))
 	}
 
 	controller.RunAll(Between(2, 3))
 
-	assert.Equal(t, n, 2)
+	assert.Equal(t, atomic.LoadInt32(&n), int32(2))
 	time.Sleep(time.Duration(10) * time.Millisecond)
-	assert.Equal(t, n, 5)
+	assert.Equal(t, atomic.LoadInt32(&n), int32(5))
 }
 
 func TestInOnly(t *testing.T) {
-	n := 0
+	var n int32
 	controller := New()
 
 	for i := 0; i < 5; i++ {
 		controller.Do(func(ctx context.Context) error {
-			n++
+			atomic.AddInt32(&n, 1)
 			return nil
 		}, P(int32(i)))
 	}
 
 	controller.Run(In([]int32{2, 4}))
 
-	assert.Equal(t, n, 2)
+	assert.Equal(t, atomic.LoadInt32(&n), int32(2))
 }
 
 func TestInAndRunAll(t *testing.T) {
-	n := 0
+	var n int32
 	controller := New()
 
 	controller.Do(func(ctx context.Context) error {
 		time.Sleep(time.Duration(10) * time.Millisecond)
-		n += 2
+		atomic.AddInt32(&n, 2)
 		return nil
 	}, P(1))
 
 	controller.Do(func(ctx context.Context) error {
 		time.Sleep(time.Duration(20) * time.Millisecond)
-		n += 3
+		atomic.AddInt32(&n, 3)
 		return nil
 	}, P(2))
 
 	controller.Do(func(ctx context.Context) error {
 		time.Sleep(time.Duration(30) * time.Millisecond)
-		n += 4
+		atomic.AddInt32(&n, 4)
 		return nil
 	}, P(3))
 
 	controller.Do(func(ctx context.Context) error {
 		time.Sleep(time.Duration(40) * time.Millisecond)
-		n += 5
+		atomic.AddInt32(&n, 5)
 		return nil
 	}, P(4))
 
 	controller.Do(func(ctx context.Context) error {
 		time.Sleep(time.Duration(50) * time.Millisecond)
-		n += 6
+		atomic.AddInt32(&n, 6)
 		return nil
 	}, P(5))
 
 	controller.RunAll(In([]int32{2, 4}))
 
-	assert.Equal(t, n, 8)
+	assert.Equal(t, atomic.LoadInt32(&n), int32(8))
 	time.Sleep(time.Duration(100) * time.Millisecond)
-	assert.Equal(t, n, 20)
+	assert.Equal(t, atomic.LoadInt32(&n), int32(20))
 }
 
 func TestWithContextButTimeout(t *testing.T) {
 	c, cancel := context.WithTimeout(context.Background(), 50*time.Millisecond)
 	defer cancel()
 
-	n := 0
+	var n int32
 	controller := New()
 
 	controller.Do(func(ctx context.Context) error {
 		time.Sleep(time.Duration(100) * time.Millisecond)
-		n++
+		atomic.AddInt32(&n, 1)
 		return nil
 	}, Name("sleep 100ms"))
 
 	err := controller.Run(WithContext(c))
 
 	assert.Equal(t, err.Error(), "context deadline exceeded")
-	assert.Equal(t, n, 0)
+	assert.Equal(t, atomic.LoadInt32(&n), int32(0))
 }
 
 func TestDone(t *testing.T) {
-	n := 0
+	var n int32
+
 	controller := New()
 
 	controller.Do(func(ctx context.Context) error {
 		time.Sleep(time.Duration(10) * time.Millisecond)
-		n++
+		atomic.AddInt32(&n, 1)
 		return nil
 	})
 
 	controller.Do(func(ctx context.Context) error {
 		time.Sleep(time.Duration(10) * time.Millisecond)
-		n++
+		atomic.AddInt32(&n, 1)
 		return nil
 	})
 
 	controller.Do(func(ctx context.Context) error {
-		n += 5
+		atomic.AddInt32(&n, 5)
 		return nil
 	})
 
 	controller.Done(func() {
-		assert.Equal(t, n, 7)
+		assert.Equal(t, atomic.LoadInt32(&n), int32(7))
 	})
 
 	controller.RunAll(Fastest())
 
-	assert.Equal(t, n, 5)
+	assert.Equal(t, atomic.LoadInt32(&n), int32(5))
 	time.Sleep(time.Duration(500) * time.Millisecond)
 }
